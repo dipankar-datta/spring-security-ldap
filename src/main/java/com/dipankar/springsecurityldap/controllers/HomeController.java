2933390c4b3bca038a26c9f287bc019f8948f181
Dipankar Datta <dattapro5@gmail.com>
2020-05-24 20:34:02 +0530

    Initial commit.

---
+++ src/main/java/com/dipankar/springsecurityldap/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.dipankar.springsecurityldap.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "<h1>Welcome to Home page</h1>";
+    }
+}
